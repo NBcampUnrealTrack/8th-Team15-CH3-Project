3395c9ff5389bee89524f1496f9f14af606817df
MYPC\kimseungha <bookcon23@gmail.com>
2026-05-11 19:26:14 +0900

    Reapply "김승하 / 날린 파일 수정"
    This reverts commit fb170a435ba44b15d3359a753b5aa2864d494705.

--- Source/TeamProject/LJW/Character/PrototypeXCharacter.h
+++ Source/TeamProject/LJW/Character/PrototypeXCharacter.h
@@ -48,16 +48,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Velocitys|Speed")
 	float Sprint_Speed;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Velocitys|Jump")
-	float Normal_Jump_Speed = 400.f;
+	float Normal_Jump_Speed;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Velocitys|Jump")
 	float Max_Jump_Speed;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Velocitys|Jump")
 	float Min_Jump_Speed;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "States|Jump")
-	bool bIsOnJumpping = false;
+	bool bIsOnJumpping;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "States|Attack")
-	bool bIsAttacking = false;
+	bool bIsAttacking;
 
 protected:
 	virtual void BeginPlay() override;
@@ -70,9 +70,6 @@ protected:
 	void Sprint_Start(const FInputActionValue& value);
 	void Sprint_Stop(const FInputActionValue& value);
 
-	void Jump_Start(const FInputActionValue& value);
-	virtual void Landed(const FHitResult& Hit) override;
-	void Jump_Stop(const FInputActionValue& value);
 	// enum =======================
 	UFUNCTION(BlueprintCallable, Category = "Mode")
 	void SetPlayerMode(EPlayerMode NewMode);
@@ -95,15 +92,6 @@ protected:
 	//EPlayerMode BeforeSetMode;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "States|Roll")
 	AActor* TargetLockActor;
-	//===============================
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "States|Use")
-	TArray<UAnimMontage*> ItemUseMontage;
-
-	void ItemUse_Start(const FInputActionValue& value);
-	bool IsItemUsing = false;
-
-	//================================
-
 public:	
 	virtual void Tick(float DeltaTime) override;
 
